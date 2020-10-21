ec2671947ea2d0664b4175918e9df1b16fcf8f13
hugohabicht01 <hugohabicht01@gmx.net>
2020-10-21 10:55:33 +0100

    Tried the googlemaps python client

--- directionsapitesting.py
+++ directionsapitesting.py
@@ -1,9 +1,9 @@
+import googlemaps
 import os
+from datetime import datetime
 from os.path import join, dirname
 from dotenv import load_dotenv
-import requests
 from pprint import pprint
-
 
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -12,10 +12,22 @@ load_dotenv(dotenv_path)
 
 api_key = os.getenv('GOOGLE_API_KEY')
 
+gmaps = googlemaps.Client(key=api_key)
 
-# url = f"https://maps.googleapis.com/maps/api/directions/json?origin=Hanau+Haendelstrasse+Germany&destination=TF107BL+UK&key={api_key}"
-url = f"https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=Haendelstrasse+Hanau+Germany&destinations=Frankfurt+Vilbeler+Landstrasse+Tankcenter&key={api_key}"
+# Geocoding an address
+# geocode_result_start = gmaps.geocode('Hanau Händelstraße 10, Deutschland')
+# geocode_result_destination = gmaps.geocode('Hanau Freiheitsplatz, Deutschland')
 
-req = requests.get(url)
+# pprint(geocode_result_start)
+# pprint(f"Destination: {geocode_result_destination}")
+# Look up an address with reverse geocoding
+# reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
-pprint(req.json())
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions('Händelstraße 10, Hanau 63452, Deutschland',
+                                     'Schlossplatz Hanau, Deutschland',
+                                     mode="driving",
+)
+
+pprint(directions_result)
