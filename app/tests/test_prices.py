63b8b5ea99cbacabeae4c6a57a32df14ae888dfe
hugohabicht01 <hugohabicht01@gmx.net>
2020-10-26 21:42:42 +0000

    Added basic dir structure, few tests and some functions

---
+++ app/tests/test_prices.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from app.petrolprices import prices
+from os.path import join, dirname
+from os import getenv
+from dotenv import load_dotenv
+
+class TestPriceFetching(TestCase):
+    def setUp(self):
+        dotenv_path = join(dirname(__file__), '.env')
+        load_dotenv(dotenv_path)
+        self.api_key = getenv('TANKERKOENING_API_KEY')
+
+    def test_getNearestStations(self):
+        res = prices.getNearestStations('50.141593', '8.898668', 1.5, "diesel", self.api_key)
+        self.assertEqual(res["ok"], True)
+        self.assertEqual(res["status"], 'ok')
