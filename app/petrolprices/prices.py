f8751f758967bc7ae0f578a2a23e0015cc3f4b87
hugohabicht01 <hugohabicht01@gmx.net>
2020-10-27 11:13:05 +0000

    #2 now following pep8

--- app/petrolprices/prices.py
+++ app/petrolprices/prices.py
@@ -1,17 +1,18 @@
 import requests
 
-def getNearestStations(lat: str, lng: str, rad: float, fueltype: str, apikey: str) -> dict:
+
+def get_nearest_stations(lat: str, lng: str, rad: float, fueltype: str, apikey: str) -> dict:
     """Fetches all petrol stations in a certain radius from a position and returns their prices"""
     PETROL_PRICES_API_URL: str = 'https://creativecommons.tankerkoenig.de/json/list.php'
     r: requests.models.Response = requests.get(PETROL_PRICES_API_URL,
-        params={"lat": lat, 
-                "lng": lng, 
-                "rad": rad, 
-                "type": fueltype, 
-                "apikey": apikey, 
-                "sort": "price"
-        }
-    )
+                                               params={"lat": lat,
+                                                       "lng": lng,
+                                                       "rad": rad,
+                                                       "type": fueltype,
+                                                       "apikey": apikey,
+                                                       "sort": "price"
+                                                       }
+                                               )
     if r.status_code != 200:
         raise RuntimeError(f"Pricing API returned http error {r.status_code}")
 
