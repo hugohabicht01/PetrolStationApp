e75ca5d3471cfe3ffe1c9758e161f302725c4ebf
hugohabicht01 <hugohabicht01@gmx.net>
2020-10-27 15:49:29 +0000

    #3 Now using google doc strings in navigation and now using the values instead of parsing text

--- app/navigation/navigation.py
+++ app/navigation/navigation.py
@@ -1,5 +1,4 @@
 import googlemaps
-import re
 from typing import List, Tuple
 
 
@@ -16,66 +15,64 @@ from typing import List, Tuple
 def get_distance_matrix(
     current_pos: Tuple[str, str], destinations: List[Tuple[str, str]], api_key: str,
 ) -> dict:
+    """
+    Gets the distance matrix from the Google Maps distance matrix API
+
+    Formats the positions and fetches the distance matrix
+
+    Args:
+        current_pos: Tuple of the latitude and longitude of the current position
+        destinations: List of the destinations, these are tuples of the latitude and longitude
+
+    Returns:
+        Response from the Google Maps API containing full address of the positions, distance, duration
+
+    Raises:
+        RuntimeError: Google Maps API returned an error
+    """
     current_coords = f"{current_pos[0]}, {current_pos[1]}"
     destinations = [f"{dest_lat}, {dest_lng}" for dest_lat, dest_lng in destinations]
 
     gmaps = googlemaps.Client(key=api_key)
 
-    return gmaps.distance_matrix(origins=current_coords, destinations=destinations)
+    gmaps_distance_matrix = gmaps.distance_matrix(origins=current_coords, destinations=destinations)
+
+    if gmaps_distance_matrix['status'] != 'OK':
+        raise RuntimeError('Google Maps API returned an error')
+
+    return gmaps_distance_matrix
 
 
 def calculate_fuel_consumption(
-    distance: float,
-    duration: str,
+    distance: int,
+    duration: int,
     avg_city_fuelconsumption: float,
     avg_motorway_fuelconsumption: float,
 ) -> float:
     """
-    @param: distance, distance that is driven
-    @param: duration, how long it takes to get there, format: "x hours y mins"
-    @param: avg_city_fuelconsumption: average fuel consumption of your car in city traffic on 100km in liters
-    @param: avg_motorway_fuelconsumption: average fuel consumption of your car on a motorway on 100km in liters
+    Estimates the fuel consumption of your car
 
     This function uses two different models, if the average speed is over 60 km/h
-    it will use the average fuel consumption for motorways, otherwise the one for 
+    it will use the average fuel consumption for motorways, otherwise the one for
     inner city traffic
+
+    Args:
+        distance: distance that is driven in meters
+        duration: how long it takes to get there in seconds
+        avg_city_fuelconsumption: average fuel consumption of your car in city traffic on 100km in liters
+        avg_motorway_fuelconsumption: average fuel consumption of your car on a motorway on 100km in liters
+
+    Returns:
+        Estimated fuel consumed in liters
     """
     # TODO: Add test
     # Get the avg. consumption on 1 km
     avg_city_fuelconsumption /= 100
     avg_motorway_fuelconsumption /= 100
 
-    # Convert
-    speed: float = distance / _parse_gmaps_travelduration(duration)
+    # Find speed in m/s and convert it to km/h
+    speed: float = (distance / duration) * 3.6
 
     if speed >= 60:
         return distance * avg_motorway_fuelconsumption
     return distance * avg_city_fuelconsumption
-
-
-def _parse_gmaps_travelduration(duration: str) -> float:
-    """
-    Possible formats(not real regular expressions!!!!!): [1-59] mins? || [1-23] hours [0-59] mins?
-    Returns the time in hours
-    """
-    # TODO: Add test
-    total_hours: float = 0
-
-    # Find the hours if existent
-    if duration.find("hour") != -1:
-        hours: int = int(re.search("([1-9][0-9]?) hours?", duration).group(1))
-        # Check for impossible results
-        if hours >= 24:
-            raise ValueError("Faulty duration")
-
-        total_hours += hours
-
-    # Find the minutes
-    minutes: int = int(re.search("([0-9][0-9]?) mins?", duration).group(1))
-
-    # Convert minutes to hours and add to total
-    # TODO: For docs, there is no need to account for minutes higher than 59,
-    # it can't overflow
-    total_hours += minutes / 60
-
-    return total_hours
