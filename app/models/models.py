7b7f30c2a23573932a3652745121077f45a09250
hugohabicht01 <hugohabicht01@gmx.net>
2020-11-11 18:39:00 +0000

    Restored the petrolstation structs, they got lost in a merge

--- app/models/models.py
+++ app/models/models.py
@@ -1,11 +1,20 @@
 import uuid
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ValidationError, validator
 from typing import List
 
 
 class Coordinate(BaseModel):
     latitude: float
     longitude: float
+
+class Fuel(BaseModel):
+    fueltype: str
+
+    @validator('fueltype')
+    def check_valid_fueltype(cls, v):
+        if v != 'diesel' or v != 'e5' or v != 'e10':
+            raise ValueError('Wrong fueltype')
+        return v
 
 
 class Distance(BaseModel):
@@ -28,8 +37,31 @@ class Row(BaseModel):
     elements: List[Element]
 
 
-class GMAPS(BaseModel):
+class GMaps_response(BaseModel):
     destination_addresses: List[str]
     origin_addresses: List[str]
     rows: List[Row]
     status: str
+
+
+class PetrolStation(BaseModel):
+    id: uuid.UUID
+    name: str
+    brand: str
+    lat: float
+    lng: float
+    dist: float
+    price: float
+    isOpen: bool
+    houseNumber: str
+    postCode: int
+
+
+
+class PetrolStations(BaseModel):
+    ok: bool
+    license: str
+    data: str
+    status: str
+    stations: List[PetrolStation]
+
