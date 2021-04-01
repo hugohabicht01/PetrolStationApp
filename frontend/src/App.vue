a773d63ce00abfdef5440f6002db1e1d2b54b489
Cedric Borgers <hugohabicht01@gmx.net>
2021-04-01 21:28:09 +0200

    Starting working on the frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <Search />
+</template>
+
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search.vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {}
+})
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
