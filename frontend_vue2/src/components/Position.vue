29dc2c11f9dba0c1b967172bcd4df66dc614700c
hugohabicht01 <hugohabicht01@gmx.net>
2021-04-05 18:56:35 +0200

    Started working on reading coordinates from GPS

---
+++ frontend_vue2/src/components/Position.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="setCurrentCoordinatesGPS">Find my current position!</button>
+    <p>Latitude: {{ currentLatitude }}, Longitude: {{ currentLongitude }}</p>
+    <iframe
+      width="600"
+      height="450"
+      style="border: 0"
+      loading="lazy"
+      allowfullscreen
+      :src="'https://www.google.com/maps/embed/v1/place?key=AIzaSyD6z4WhJMY85Xgc9ZIRTMbCgr1zfSIMhUg&q='+formattedCoords"
+    >
+    </iframe>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex';
+
+export default {
+  computed: mapGetters(['currentLatitude', 'currentLongitude', 'formattedCoords']),
+  methods: mapMutations(['setCurrentCoordinatesGPS']),
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
