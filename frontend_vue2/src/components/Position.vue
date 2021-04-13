1e24d14e2904da053b518d9a979ff88a4e6c64b0
hugohabicht01 <hugohabicht01@gmx.net>
2021-04-13 09:55:38 +0100

    Refractored setting coordinates

--- frontend_vue2/src/components/Position.vue
+++ frontend_vue2/src/components/Position.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <button @click="setCurrentCoordinatesGPS">Find my current position!</button>
+    <button @click="setPosGPS">Find my current position!</button>
+    <div>or</div>
+    <p>Put in your current coordinates manually</p>
+    <p>
+      Latitude: <input type="text" v-model="currLat" />, Longitude:
+      <input type="text" v-model="currLng" />
+    </p>
+    <button @click="setPosManually(currLat, currLng)">Set position</button>
     <p>Latitude: {{ currentLatitude }}, Longitude: {{ currentLongitude }}</p>
     <iframe
       width="600"
@@ -8,18 +15,47 @@
       style="border: 0"
       loading="lazy"
       allowfullscreen
-      :src="'https://www.google.com/maps/embed/v1/place?key=AIzaSyD6z4WhJMY85Xgc9ZIRTMbCgr1zfSIMhUg&q='+formattedCoords"
+      :src="
+        'https://www.google.com/maps/embed/v1/place?key=AIzaSyD6z4WhJMY85Xgc9ZIRTMbCgr1zfSIMhUg&zoom=5&q=' +
+        formattedCoords
+      "
     >
     </iframe>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex';
+import { mapGetters } from 'vuex';
+import store from '../store/index';
 
 export default {
+  data: () => ({
+    currLat: 0,
+    currLng: 0,
+  }),
   computed: mapGetters(['currentLatitude', 'currentLongitude', 'formattedCoords']),
-  methods: mapMutations(['setCurrentCoordinatesGPS']),
+  methods: {
+    setPosGPS: () => {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          const coords = {
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+            error: false,
+          };
+          store.commit('setCurrentCoordinates', coords);
+        },
+        (err) => {
+          console.log(`Error getting current coordinates. ERROR: ${err}`);
+          const coords = { latitude: null, longitude: null, error: true };
+          store.commit('setCurrentCoordinates', coords);
+        },
+      );
+    },
+    setPosManually: (lat, lng) => {
+      store.commit('setCurrentCoordinates', { latitude: lat, longitude: lng, error: false });
+    },
+  },
 };
 </script>
 
--- frontend_vue2/src/store/index.js
+++ frontend_vue2/src/store/index.js
@@ -3,25 +3,19 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
     currentCoordinates: {
       latitude: 123,
       longitude: 456,
-      accuracy: null,
       err: false,
     },
   },
   mutations: {
-    setCurrentCoordinatesGPS(state) {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        state.currentCoordinates.latitude = pos.coords.latitude;
-        state.currentCoordinates.longitude = pos.coords.longitude;
-        state.currentCoordinates.accuracy = pos.coords.accuracy;
-      }, (err) => {
-        console.log(`Error getting current coordinates. ERROR: ${err}`);
-        state.currentCoordinates.err = true;
-      });
+    setCurrentCoordinates(state, coords) {
+      state.currentCoordinates.latitude = coords.latitude;
+      state.currentCoordinates.longitude = coords.longitude;
+      state.currentCoordinates.err = coords.error;
     },
   },
   getters: {
@@ -34,3 +28,5 @@ export default new Vuex.Store({
   modules: {
   },
 });
+
+export default store;
