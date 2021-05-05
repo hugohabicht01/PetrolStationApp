f9ce1900005d5b1fcb3d62a547a86ce0e39be9db
hugohabicht01 <hugohabicht01@gmx.net>
2021-05-05 11:50:37 +0100

    SUBMITTED coursework

--- frontend_vue2/src/main.js
+++ frontend_vue2/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue';
+import * as VueGoogleMaps from 'gmap-vue';
 import App from './App.vue';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'YOUR_API_TOKEN',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  },
+
+  /// / If you intend to programmatically custom event listener code
+  /// / (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+  /// / instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+  /// / you might need to turn this on.
+  // autobindAllEvents: false,
+
+  /// / If you want to manually install components, e.g.
+  /// / import {GmapMarker} from 'gmap-vue/src/components/marker'
+  /// / Vue.component('GmapMarker', GmapMarker)
+  /// / then set installComponents to 'false'.
+  /// / If you want to automatically install all the components this property must be set to 'true':
+  installComponents: true,
+});
 
 new Vue({
   store,
