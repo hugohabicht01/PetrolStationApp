a33dede1666120cbc6916ae8cb8ef1721cf96e02
hugohabicht01 <hugohabicht01@gmx.net>
2021-07-29 23:03:03 +0200

    Updated some boilerplate files

--- frontend_vue2/src/store/index.js
+++ frontend_vue2/src/store/index.js
@@ -24,7 +24,7 @@ const store = new Vuex.Store({
       avgConsumptionCity: 12.0,
       avgConsumptionMotorway: 7.2,
     },
-    apiURL: 'http://34.68.127.57',
+    apiURL: 'https://imposing-water-291212.ew.r.appspot.com',
   },
   mutations: {
     setCurrentCoordinates(state, coords) {
